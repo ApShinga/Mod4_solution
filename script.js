728ca19ad55e824ad1a273daf2ea0f5a655c0916
Aphiwe Shinga <79520238+ApShinga@users.noreply.github.com>
2021-03-07 20:29:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@
 // STEP 1:
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
-// (Note, Step 2 will be done in the SpeakHello.js file.)
+// (Note, Step 2 will be done in the Hello.js file.)
 
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
